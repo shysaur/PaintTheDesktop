27ae675ea21d43bd6965f55b700c8bd8718c9944
Daniele Cattaneo <daniele.cattaneo@alice.it>
2021-05-18 17:34:41 +0200

    Add tabs to the preferences window.

--- PaintTheDesktop/PTDPreferencesWindowController.m
+++ PaintTheDesktop/PTDPreferencesWindowController.m
@@ -28,7 +28,24 @@
 #import "PTDAppDelegate.h"
 
 
+@interface PTDPreferencesWindowController ()
+
+@property (nonatomic, weak) IBOutlet NSTabView *preferencesTabView;
+@property (nonatomic, weak) IBOutlet NSToolbar *toolbar;
+
+@end
+
+
 @implementation PTDPreferencesWindowController
+
+
++ (void)initialize
+{
+  NSUserDefaults *ud = NSUserDefaults.standardUserDefaults;
+  [ud registerDefaults:@{
+    @"PTDPreferencesTab": @(0)
+  }];
+}
 
 
 - (NSString *)windowNibName
@@ -40,6 +57,12 @@
 - (void)windowDidLoad
 {
   self.window.canHide = NO;
+  
+  NSUserDefaults *ud = NSUserDefaults.standardUserDefaults;
+  int tab = (int)[ud integerForKey:@"PTDPreferencesTab"];
+  int maxTabs = (int)self.preferencesTabView.numberOfTabViewItems;
+  tab = MAX(0, MIN(tab, maxTabs - 1));
+  [self showTabAtIndex:tab withAnimation:NO keepXCenter:YES];
 }
 
 
@@ -47,6 +70,46 @@
 {
   [PTDAppDelegate.appDelegate pushAppShouldShowInDock];
   [super showWindow:sender];
+}
+
+
+- (IBAction)changeTab:(id)sender
+{
+  const char *ident = [sender itemIdentifier].UTF8String;
+  int tabId = -1;
+  sscanf(ident, "%d", &tabId);
+  
+  if (tabId < 0 && tabId >= self.preferencesTabView.numberOfTabViewItems) {
+    NSBeep();
+    return;
+  }
+  
+  [self showTabAtIndex:tabId withAnimation:YES keepXCenter:NO];
+  
+  NSUserDefaults *ud = NSUserDefaults.standardUserDefaults;
+  [ud setInteger:tabId forKey:@"PTDPreferencesTab"];
+}
+
+
+- (void)showTabAtIndex:(int)tabId withAnimation:(BOOL)anim keepXCenter:(BOOL)kxc
+{
+  NSTabViewItem *tvi = [self.preferencesTabView tabViewItemAtIndex:tabId];
+  
+  NSSize fittingSize = [tvi.view fittingSize];
+  NSRect currentFrame = self.window.frame;
+  NSRect newFrame = [self.window frameRectForContentRect:(NSRect){NSZeroPoint, fittingSize}];
+  newFrame.origin.x = currentFrame.origin.x;
+  if (kxc)
+    newFrame.origin.x -= (newFrame.size.width - currentFrame.size.width) / 2;
+  newFrame.origin.y = NSMaxY(currentFrame) - newFrame.size.height;
+  
+  self.preferencesTabView.hidden = YES;
+  [self.preferencesTabView selectTabViewItem:tvi];
+  [self.window setFrame:newFrame display:YES animate:anim];
+  self.preferencesTabView.hidden = NO;
+  
+  NSString *ident = [NSString stringWithFormat:@"%d", tabId];
+  self.toolbar.selectedItemIdentifier = ident;
 }
 
 
