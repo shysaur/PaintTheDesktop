e8067e404bd12392103e4709073942997bf36a41
Daniele Cattaneo <daniele.cattaneo@alice.it>
2021-05-19 21:56:33 +0200

    Fix transparency blend function in PTDPaintView.
    Fixes an issue where selection indicators would draw differently than the canvas.

--- PaintTheDesktop/PTDPaintView.m
+++ PaintTheDesktop/PTDPaintView.m
@@ -294,7 +294,8 @@
   
   glEnable(GL_ALPHA_TEST);
   glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
+  glBlendFuncSeparate(GL_ONE, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_SCISSOR_TEST);
   glDisable(GL_DITHER);
